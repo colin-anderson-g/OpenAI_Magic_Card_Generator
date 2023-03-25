3fbfa62e03bce3fce9d8f5b4230cdf6e7321bd1b
luckymello <107649920+luckymello@users.noreply.github.com>
2023-03-24 23:48:29 -0400

    Add files via upload

--- generate.py
+++ generate.py
@@ -1,11 +1,20 @@
 import openai_text_utils
 import openai_image_utils
+import card_arguments
 
-text_prompt = """Create a new randomly generated Magic the Gathering card name TrueNear. Format as a json."""
-
-openai_text_utils.generate(text_prompt)
+# openai_text_utils.generateWithArguments(card_arguments.CardArguments("True Near")) 
+# it can still be called like this using just the name of the card
 
 image_prompt = """Create art for a Magic the Gathering card named TrueNear"""
 
 openai_image_utils.generate(image_prompt)
+
+with open("input.txt", "r") as input_file:
+    num_lines = sum(1 for line in open('input.txt'))
+    i = 1
+    for line in input_file:
+        arguments = card_arguments.CardArguments(line)
+        openai_text_utils.generateWithArguments(arguments)
+        print("finsihed with " + str(i) + "/" + str(num_lines) + "\n")
+        i += 1
 
--- openai_text_utils.py
+++ openai_text_utils.py
@@ -1,16 +1,38 @@
 import api_gen_properties
 import openai
+import card_arguments
 
 openai.api_key = api_gen_properties.API_KEY
 
-def generate(prompt):
-    request = build_request(prompt)
-    response = send_request(request)
-    output_file_path = "output.txt"
-    with open(output_file_path, "w") as output_file:
-        output_file.write(response.choices[0].text)
-    print(f"Response saved to {output_file_path}")
+prompt = """Create a new randomly generated Magic the Gathering card with the name """
+json_format = """\nCreate a valid json object for the card into the following format:
+{
+    "Name": "Card Name",
+    "Rarity": "Rarity of Card",
+    "Type": "The card Types in an array",
+    "Manacost": "The manacost of the card with colors",
+    "CMC": The converted manacost of the card,
+    "Supertype": "The supertype of the card",
+    "Subtype": "The subtypes of the card in an array",
+    "Colors": "The colors of the card in an array",
+    "Power": The power of the card,
+    "Toughness": The toughness of the card,
+    "Loyalty": The starting loyalty of the card,
+    "Art-Description": "The description of the art of the card",
+    "Abilies": "The abilities of the card in an array",
+    "Flavor-Text": "The flavor text of the card",
+}"""
 
+output_file_path = "output.txt"
+output_file = open(output_file_path, "w")
+
+def generateWithArguments(cardarguments):
+    print(cardarguments.createStringArgument())
+    input = prompt + cardarguments.createStringArgument() + json_format
+    request = build_request(input)
+    response = send_request(request)
+    output_file.write(response.choices[0].text)
+    print(f"Response saved to {output_file_path}")
 
 def build_request(prompt, model="text-davinci-002", max_tokens=1024, temperature=0.5):
     request = {
@@ -21,7 +43,7 @@ def build_request(prompt, model="text-davinci-002", max_tokens=1024, temperature
     }
     return request
 
-
 def send_request(request):
     response = openai.Completion.create(**request)
     return response
+
